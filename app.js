db8d6db8fbbc79f241e2430ca0d3ad9ab7a3d7b9
deliacodes <deliacodes@gmail.com>
2017-10-29 21:17:07 -0700

    initial html structure. small app change.

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-var jquery = require('jquery');
 "use strict";
 
 //first is the google maps api and then is the trimet transit trip planner api
@@ -6,7 +5,7 @@ var jquery = require('jquery');
 //Google Maps Directions
 //comment everything and uncomment line by line and run those uncommented
 //6 lines of code in your head at the time - methods short for this reason
-const MAPS_DIRECTIONS_URL = 'https://maps.googleapis.com/maps/api/js?key=AIzaSyCndF3UNkH3RVb1fW8S65GmGDyHIso4uB0&callback=initMap';
+//const MAPS_DIRECTIONS_URL = 'https://maps.googleapis.com/maps/api/js?key=AIzaSyCndF3UNkH3RVb1fW8S65GmGDyHIso4uB0&callback=initMap';
 
 
 
@@ -15,21 +14,21 @@ const MAPS_DIRECTIONS_URL = 'https://maps.googleapis.com/maps/api/js?key=AIzaSyC
 const TRIP_PLANNER_URL = 'https://developer.trimet.org/ws/V1/trips/tripplanner';
 
 
-function getDataFromTripPlanner ( start, destination1 , callback) {
+function getDataFromTripPlanner(start, destination1, callback) {
   var query = {
-    'path' : '',
-    'appID' : ''
+    'path': '',
+    'appID': ''
     //add destination and start
   };
-  
+
   $.getJSON(TRIP_PLANNER_URL, query, callback);
 }
 
-function listenInputExecute (){
+function listenInputExecute() {
 
-$('button').click(function(){
-  var destination1 = $('#waypoint1').val();
-  var start = $('#begin').val();
+  $('button').click(function () {
+    var destination1 = $('#waypoint1').val();
+    var start = $('#begin').val();
   });
   getDataFromTripPlanner(start, destination1);
 }
