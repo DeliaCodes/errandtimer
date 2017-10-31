f40128e85cfbf20bbe2cd423eff042d14c30d774
deliacodes <deliacodes@gmail.com>
2017-10-30 22:23:03 -0700

    roughed out process and display functions.

--- app.js
+++ app.js
@@ -4,27 +4,39 @@
 const TRIP_PLANNER_URL = 'https://developer.trimet.org/ws/V1/trips/tripplanner';
 
 //API call
-function getDataFromTripPlanner(start, errand1, callback) {
+function getDataFromTripPlanner(start, errand1, date, time, callback) {
   var query = {
-    'path': '',
-    'appID': ''
-    //add destination and start
+    'path': 'https://developer.trimet.org/ws/V1/trips/tripplanner',
+    'appID': '1DA0BDD6A9377B0851E84C412',
+    'date' : 'date',
+    'time' : 'time',
+    'fromPlace' : 'start',
+    'toPlace' : 'errand1'
   };
 
   $.getJSON(TRIP_PLANNER_URL, query, callback);
 }
 
-//process data
+//process data function goes here
+function processData (data) {
+console.log(data);
+}
+
+function displayData (data) {
+processData(data);
+}
 
 
 function listenInputExecute() {
 
-  $('button').click(function () {
+  $('#whichErrand').submit(function () {
+    event.preventDefault();
     var errand1 = $('#errand1').val();
     var start = $('#start').val();
+    var date = $('#tripDate').val();
+    var time = $('#startTime').val();
   });
-  getDataFromTripPlanner(start, errand1);
-
+  getDataFromTripPlanner(start, errand1, date, time, displayData);
 }
 
-$(listenInputExecute)
+$(listenInputExecute);
