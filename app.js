25a4db47168d462bb43574c1a404d4f0cfb71b26
deliacodes <deliacodes@gmail.com>
2017-11-18 18:43:24 -0800

    ported everything over to app.js; working on unbreaking more than one hour times

--- app.js
+++ app.js
@@ -1,48 +1,80 @@
-"use strict";
+	"use strict";
+		
+		var	trip = {}
 
-function initMap () {
-var directionsService = new google.maps.DirectionsService;
-var request = {
-  origin : start,
-  destination : start,
-  waypoints : errand1,
-  optimizeWaypoints: true,
-travelMode : 'DRIVING'
-}
-directionsService.route(request, callBack);
-}
+		//starts the process - impure d/t jquery
+		function initMap() {
+			$(getInput);
+			
+		}
 
-function callBack (response, status) {
-  if (status === 'OK') {
-    console.log(response);
-    alert('ok!');
-  }
-  else {alert('error!');}
-}
+const tripAddressesToDisplay = (start,errand1,trip) => {
+	trip.origin= start;
+	trip.errand = errand1;
+	}
 
-//process data function 
-function processData(data) {
-  console.log(data);
-}
+		//gets input from user - impure
+		const getInput = () => {
+			$('#errandForm').submit(function (event) {
+				event.preventDefault();
+				var errand1 = $('#errand1').val()
+				var start = $('#start').val();
+				tripAddressesToDisplay(start,errand1,trip);
+				route(start, addErrand(errand1));
+			});
+		}
+
+		//adds errand to to object - pure
+		const addErrand = (input) => [{
+			location: input
+		}]
+
+		//makes the route request - impure d/t the google maps
+		function route(start, errand1) {
+			var request = {
+				origin: start,
+				destination: start,
+				waypoints: errand1,
+				optimizeWaypoints: true,
+				travelMode: 'DRIVING'
+			}
+			//does this need to be here? or can this be broken out?
+			var directionsService = new google.maps.DirectionsService;
+			directionsService.route(request, callBack);
+		}
+
+		//success of response execution - impure
+		function callBack(response, status) {
+			if (status === 'OK') {
+				//console.log(response);
+				//alert('ok!');
+				const totalDuration = processData(response);
+				displayData(totalDuration);
+			} 
+			}
+		
+//adds times for each leg to the trip object
+		const legTimesToDisplay = (leg1, leg2, trip) => {
+		trip.legTimes = [leg1, leg2];
+		console.log (trip);
+		}
+
+  const getLegTimeEstimate - ()
 
 
-//displays data to user
-function displayData(data) {
-  processData(data);
-}
+		//process data function - to do - loop through the data object and add map + reduce, map get all the legs.duration and then filter. instead of process use the value
+		const processData = (data) => {
+			//console.log(data);
+			let leg1Human = data.routes[0].legs[0].duration.text;
+      let leg2Human = data.routes[0].legs[1].duration.text;
+      
+			let leg1Time= data.routes[0].legs[0].duration.value;
+			let leg1Time= data.routes[1].legs[0].duration.value;
+			legTimesToDisplay(leg1Human,leg2Human,trip);
+		return +leg1 + +leg2;
+		}
 
-//gets input from user
-function getInput () {
-  $('#errandForm').submit(function () {
-    event.preventDefault();
-    var errand1 =[];
-    errand1.push($('#errand1').val());
-    var start = $('#start').val();
-  });
-}
-
-function listenInputExecute() {
-getInput();
-}
-
-$(listenInputExecute);
+		//displays data to user - impure - add detail about your errands like which one.
+		const displayData = (totalDuration) => {
+			$('#results').append('<p> The total duration of your trip from ' + trip.origin  + ' to your errand ' + trip.errand + ' and back  is ' + totalDuration + ' minutes.</p>' +'<p>From ' + trip.origin + ' to ' + trip.errand + ' will take ' + trip.legTimes[0] + ' minutes.</p><p> From ' + trip.errand + ' back to ' + trip.origin + ' will take ' + trip.legTimes[1] + ' minutes.</p>');
+		}
