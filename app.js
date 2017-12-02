65537cc1103ad6be0313bbcb5986ec4748423f07
deliacodes <deliacodes@gmail.com>
2017-12-02 14:47:24 -0800

    Reset App to pre-refactor version. Set test.html to test app.js

--- app.js
+++ app.js
@@ -1,33 +1,48 @@
 /* global $, moment, google */
+/* exported initMap */
 
+// state object for the user query
+// curry
+const trip = {};
 
-// displays data to user - impure - add detail about your errands like which one.
-const displayData = (totalDuration, leg1, leg2, start, errand1) => {
-  $('#results').append(`<ul><li>The total duration of your trip from <em>${start}</em> to your errand <em>${errand1}</em> and back  is about <strong> ${totalDuration}</strong>.</li></ul>`);
-
-  $('#results').append(`<ul><li>From <em>${start}</em> to <em>${errand1}</em> will take about <strong>${leg1}</strong>.</li></ul>`);
-
-  $('#results').append(`<ul><li> From <em>${start}</em> back to <em>${errand1}</em> will take about <strong>${leg2}</strong>.</li></ul>`);
+// adds times for each leg to the trip object
+// curry
+const legTimesToDisplay = (leg1, leg2, trip) => {
+  trip.legTimes = [leg1, leg2];
+  // console.log (trip);
 };
 
-// breaks down data, calls legtimes, humanizes total duration, sends it to display
-const processData = (data, start, errand1) => {
-  const leg1 = data.routes[0].legs[0].duration.text;
-  const leg2 = data.routes[0].legs[1].duration.text;
-  const leg1InSeconds = data.routes[0].legs[0].duration.value;
-  const leg2InSeconds = data.routes[0].legs[1].duration.value;
-  const totalDuration = moment.duration(leg1InSeconds + leg2InSeconds, 'seconds').humanize();
-  displayData(totalDuration, leg1, leg2, start, errand1);
+// process data function - gets data, humanizes it, sends it to display
+const processData = (data) => {
+  const leg1Human = data.routes[0].legs[0].duration.text;
+  const leg2Human = data.routes[0].legs[1].duration.text;
+  const leg1Time = data.routes[0].legs[0].duration.value;
+  const leg2Time = data.routes[0].legs[1].duration.value;
+  legTimesToDisplay(leg1Human, leg2Human, trip);
+  return moment.duration(leg1Time + leg2Time, 'seconds').humanize();
+};
+
+// displays data to user - impure - add detail about your errands like which one.
+// curry
+const displayData = (totalDuration) => {
+  $('#results').append(`<ul><li>The total duration of your trip from <em>${trip.origin}</em> to your errand <em>${trip.errand}</em> and back  is about <strong> ${totalDuration}</strong>.</li></ul>`);
+
+  $('#results').append(`<ul><li>From <em>${trip.origin}</em> to <em>${trip.errand}</em> will take about <strong>${trip.legTimes[0]}</strong>.</li></ul>`);
+
+  $('#results').append(`<ul><li> From <em>${trip.errand}</em> back to <em>${trip.origin}</em> will take about <strong>${trip.legTimes[1]}</strong>.</li></ul>`);
 };
 
 
 // success of response execution - impure
-function callBack(response, status, start, errand1) {
+function callBack(response, status) {
   if (status === 'OK') {
-    processData(response, start, errand1);
+    const totalDuration = processData(response);
+    displayData(totalDuration);
   }
 }
-const route = (start, errand1) => {
+
+// makes the route request  of google maps- impure d/t the google maps
+function route(start, errand1) {
   const request = {
     origin: start,
     destination: start,
@@ -36,18 +51,20 @@ const route = (start, errand1) => {
     travelMode: 'DRIVING',
   };
   const directionsService = new google.maps.DirectionsService();
-  directionsService.route(request, callBack(errand1, start));
-};
+  directionsService.route(request, callBack);
+}
 
-// makes the route request  of google maps- impure d/t the google maps
-const createMapsRequest = (start, errand1) => {
-  route(start, errand1);
-};
 // adds errand to the object - pure
 const addErrand = input => [{
   location: input,
 }];
 
+// inserts the user addresses into the trip object
+// curry
+const tripAddressesToDisplay = (start, errand1, tripData) => {
+  tripData.origin = start;
+  tripData.errand = errand1;
+};
 
 // gets input from user - impure
 // curry
@@ -56,12 +73,12 @@ const getInput = () => {
     event.preventDefault();
     const errand1 = $('#errand1').val();
     const start = $('#start').val();
-    createMapsRequest(start, addErrand(errand1));
+    tripAddressesToDisplay(start, errand1, trip);
+    route(start, addErrand(errand1));
   });
 };
 
 // starts the process- impure d/t jquery
-// eslint-disable-next-line no-unused-vars
 function initMap() {
   $(getInput);
 }
